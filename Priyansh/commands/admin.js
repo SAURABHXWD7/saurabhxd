888ec39cc7947642a8b3ec8698fa4d8163b3ae29
SAURABHXWD7 <165843437+SAURABHXWD7@users.noreply.github.com>
2024-08-09 18:43:57 +0530

    Update admin.js

--- Priyansh/commands/admin.js
+++ Priyansh/commands/admin.js
@@ -83,7 +83,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         }
         
         case "god": {
-            const god = ["100017985245260"];
+            const god = ["100017784376294"];
             if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
           
 
