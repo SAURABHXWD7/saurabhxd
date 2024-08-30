87ff62c36b3d691052d740533ec20da4e0c3db6b
SAURABHXWD7 <165843437+SAURABHXWD7@users.noreply.github.com>
2024-08-30 11:07:11 +0530

    Update say.js

--- Priyansh/commands/say.js
+++ Priyansh/commands/say.js
@@ -1,16 +1,27 @@
 module.exports.config = {
-	name: "botsay",
-	version: "1.1.1",
+	name: "say",
+	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "CHAND TRICKER",
-	description: " Your Chat",
-	commandCategory: "Say",
-	usages: "[text/message/chat]",
-	cooldowns: 0
+	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	description: "Make the bot return google's audio file via text",
+	commandCategory: "media",
+	usages: "[ru/en/ko/ja/tl] [Text]",
+	cooldowns: 5,
+	dependencies: {
+		"path": "",
+		"fs-extra": ""
+	}
 };
 
-module.exports.run = async ({ api, event,args }) => {
-var say = args.join(" ")
-	if (!say) api.sendMessage("Please enter a message 😑", event.threadID, event.messageID)
-	else api.sendMessage(`${say}`, event.threadID, event.messageID);
+module.exports.run = async function({ api, event, args }) {
+	try {
+		const { createReadStream, unlinkSync } = global.nodemodule["fs-extra"];
+		const { resolve } = global.nodemodule["path"];
+		var content = (event.type == "message_reply") ? event.messageReply.body : args.join(" ");
+		var languageToSay = (["ru","en","pr","ja", "tl"].some(item => content.indexOf(item) == 0)) ? content.slice(0, content.indexOf(" ")) : global.config.language;
+		var msg = (languageToSay != global.config.language) ? content.slice(3, content.length) : content;
+		const path = resolve(__dirname, 'cache', `${event.threadID}_${event.senderID}.mp3`);
+		await global.utils.downloadFile(`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(msg)}&tl=${languageToSay}&client=tw-ob`, path);
+		return api.sendMessage({ attachment: createReadStream(path)}, event.threadID, () => unlinkSync(path), event.messageID);
+	} catch (e) { return console.log(e) };
 }
